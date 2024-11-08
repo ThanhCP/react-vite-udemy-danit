06d2f8bca35475dea1d50d03512367f837864f74
Phạm Chí Thành <thanhpc.hl@gmail.com>
2024-11-08 16:24:00 +0700

    Delete user ok

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,19 +1,36 @@
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
-import { Space, Table, Tag } from "antd";
+import { Space, Table, Tag, Popconfirm, notification } from "antd";
 import UpdateUserModal from "./update.user";
 import { useState } from "react";
 import ViewUserDetail from "./view.user.details";
+import { deleteUserAPI } from "../../service/api.service";
 
 function UserTable(props) {
-  const { dataUser,loadUser } = props;
+  const { dataUser, loadUser } = props;
 
   const [isModalUpdate, setIsModalUpdate] = useState(false);
 
-  const [dataUpdate, setDataUpdate] = useState('')
+  const [dataUpdate, setDataUpdate] = useState("");
 
-  const [isDetailOpen, setIsDetailOpen] = useState(false)
-  
-  const [dataDetail, setDataDetails] = useState('')
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+
+  const [dataDetail, setDataDetails] = useState("");
+
+  const handleDeleteUser = async (id) => {
+    const res = await deleteUserAPI(id);
+    if (res.data) {
+      notification.success({
+        message: "Delete user",
+        description: "Xóa user thành công",
+      });
+      await loadUser();
+    } else {
+      notification.error({
+        message: "Error delete user",
+        description: JSON.stringify(res.message),
+      });
+    }
+  };
 
   const columns = [
     {
@@ -23,13 +40,12 @@ function UserTable(props) {
         <>
           <a
             onClick={() => {
-              setDataDetails(record)
-              setIsDetailOpen(true)
+              setDataDetails(record);
+              setIsDetailOpen(true);
             }}
           >
-          {record._id}
+            {record._id}
           </a>
-          
         </>
       ),
     },
@@ -49,14 +65,24 @@ function UserTable(props) {
       title: "Action",
       key: "action",
       render: (_, record) => (
-        <div style={{display:"flex", gap: "20px"}}>
-          <EditOutlined 
-            onClick={() =>{
+        <div style={{ display: "flex", gap: "20px" }}>
+          <EditOutlined
+            onClick={() => {
               setDataUpdate(record);
-              setIsModalUpdate(true)
+              setIsModalUpdate(true);
             }}
-          style={{cursor: "pointer", color: "orange"}} /> 
-          <DeleteOutlined style={{cursor: "pointer", color: "red"}} />
+            style={{ cursor: "pointer", color: "orange" }}
+          />
+          <Popconfirm
+            title="Xóa người dùng"
+            description="Bạn chắc chắn xóa user này ?"
+            onConfirm={() => handleDeleteUser(record._id)}
+            okText="Yes"
+            cancelText="No"
+            placement="left"
+          >
+            <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+          </Popconfirm>
         </div>
       ),
     },
@@ -67,19 +93,19 @@ function UserTable(props) {
       <Table columns={columns} dataSource={dataUser} rowKey={"_id"} />;
       <UpdateUserModal
         isModalUpdate={isModalUpdate}
-        setIsModalUpdate = {setIsModalUpdate}
-        dataUpdate = {dataUpdate}
-        setDataUpdate = {setDataUpdate}
+        setIsModalUpdate={setIsModalUpdate}
+        dataUpdate={dataUpdate}
+        setDataUpdate={setDataUpdate}
         loadUser={loadUser}
       />
       <ViewUserDetail
         isDetailOpen={isDetailOpen}
         setIsDetailOpen={setIsDetailOpen}
         dataDetail={dataDetail}
-        setDataDetails={setDataDetails} 
+        setDataDetails={setDataDetails}
       />
     </>
-  )
+  );
 }
 
 export default UserTable;
--- src/service/api.service.jsx
+++ src/service/api.service.jsx
@@ -25,9 +25,13 @@ const updateUserAPI = (_id, fullName, phoneNumber) =>{
 
 const fetchAllUserAPI = () =>{
     const URL_BACKEND = '/api/v1/user';
-    
     return axios.get(URL_BACKEND)
 }
 
+const deleteUserAPI = (id) =>{
+    const URL_BACKEND = `/api/v1/user/${id}`;
+    return axios.delete(URL_BACKEND)
+}
 
-export {createUserAPI,fetchAllUserAPI,updateUserAPI};
+
+export {createUserAPI,fetchAllUserAPI,updateUserAPI, deleteUserAPI};
